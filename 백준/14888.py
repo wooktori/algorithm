53f1ad3d95885d7d6a941215668d144c4cb4fc59
wooktori <jjun9503@gmail.com>
2024-11-15 19:42:18 +0900

    11/15 문제풀이

--- 백준/14888.py
+++ 백준/14888.py
@@ -29,3 +29,43 @@ def func(plus, minus, mul, div, count, sum):
 func(plus, minus, mul, div, 1, nums[0])
 print(maxV)
 print(minV)
+
+#11/15
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+nums = list(map(int, input().split()))
+plus, minus, mul, div = map(int, input().split())
+maxV = int(-1e9)
+minV = int(1e9)
+
+def dfs(i, now):
+    global plus, minus, mul, div, maxV, minV
+    if i == N:
+        maxV = max(maxV, now)
+        minV = min(minV, now)
+        return
+    
+    else:
+        if plus>0:
+            plus-=1
+            dfs(i+1, now + nums[i])
+            plus+=1
+        if minus > 0:
+            minus -= 1
+            dfs(i+1, now - nums[i])
+            minus += 1
+        if mul > 0:
+            mul -= 1
+            dfs(i+1, now * nums[i])
+            mul += 1
+        if div > 0:
+            div -= 1
+            dfs(i+1, int(now / nums[i]))
+            div += 1
+        
+
+dfs(1,nums[0])
+print(maxV)
+print(minV)
