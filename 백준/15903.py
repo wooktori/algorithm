a9ed329a485359098e8f4aa99bdc3fd76e023021
wooktori <jjun9503@gmail.com>
2024-11-30 18:08:40 +0900

    11/30 문제풀이

---
+++ 백준/15903.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+
+for i in range(m):
+    arr.sort()
+    temp = arr[0] + arr[1]
+    arr[0] = temp
+    arr[1] = temp
+
+print(sum(arr))
