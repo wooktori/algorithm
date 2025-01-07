610199fe6866fe83b909b96e6444b062f0f124b4
wooktori <jjun9503@gmail.com>
2025-01-07 22:04:31 +0900

    01/07 문제풀이 주석 추가

--- 프로그래머스/LV2/주차 요금 계산.py
+++ 프로그래머스/LV2/주차 요금 계산.py
@@ -1,23 +1,32 @@
 def solution(fees, records):
-    answer = {i: fees[1] for i in set([i[6:10] for i in records])}
-    sorted_answer = dict(sorted(answer.items()))
-    dic = {}
-    for i in records:
-        time = int(i[:2]) * 60 + int(i[3:5])
-        if i[-2:] == "IN":
-            if i[6:10] in dic:
-                dic[i[6:10]] -= time
-            else:
-                dic[i[6:10]] = -time
-        else:
-            dic[i[6:10]] += time
+    answer = {
+        i: fees[1] for i in set([i[6:10] for i in records])
+    }  # <차량번호 : 기본요금> 을 키와 값으로 갖는 딕셔너리 생성.
+    sorted_answer = dict(sorted(answer.items()))  # 차량번호를 기준으로 정렬.
+    dic = {}  # <차량번호 : 누적 주차 시간> 을 키와 값으로 갖는 딕셔너리.
 
+    # 총 누적시간 담기.
+    for i in records:
+        time = int(i[:2]) * 60 + int(
+            i[3:5]
+        )  # 입/출차 기록에서 시각의 위치는 항상 일정. 해당 인덱스를 분으로 변환해서 변수에 할당.
+        car_num = i[6:10]
+        if i[-2:] == "IN":  # 입차일 경우
+            if car_num in dic:  # 이미 값이 존재하는 경우와 아닌 경우.
+                dic[car_num] -= time
+            else:
+                dic[car_num] = -time
+        else:  # 출차일 경우
+            dic[car_num] += time
+
+    # 입차 기록만 있고 출차 기록은 없는 예외 처리.
     for i in dic:
         if dic[i] <= 0:
             dic[i] += 1439
 
+    # 누적 시간을 바탕으로 금액 계산.
     for i in dic:
-        a_time = dic[i] - fees[0]
+        a_time = dic[i] - fees[0]  # 누적 시간에서 기본 시간을 제외한 추가 시간.
         if a_time > 0:
             if a_time % fees[2] == 0:
                 sorted_answer[i] += (dic[i] - fees[0]) // fees[2] * fees[3]
--- 프로그래머스/LV2/파일명 정렬.py
+++ 프로그래머스/LV2/파일명 정렬.py
@@ -1,24 +1,31 @@
 def solution(files):
     answer = []
+
     for file in files:
         head, number, tail = "", "", ""
-        flag = False
+        flag = False  # flag를 이용해 세 부분 나누기.
+
         for i in range(len(file)):
-            if flag == True and file[i].isdigit() == False:
+            if (
+                flag == True and file[i].isdigit() == False
+            ):  # number 구간이 지나고 나온 바로 다음 문자부터 끝까지 tail에 추가.
                 tail = file[i:]
                 break
-            if file[i].isdigit() == False:
+            if file[i].isdigit() == False:  # 숫자가 아니라면 head에 추가.
                 head += file[i]
-            else:
-                flag = True
+            else:  # 숫자라면 number에 추가.
+                flag = True  # flag 값을 True로 바꿔 tail을 구분할 수 있도록 함.
                 number += file[i]
 
         answer.append([head, number, tail])
-    answer.sort(key=lambda x: (x[0].lower(), int(x[1])))
+
+    answer.sort(key=lambda x: (x[0].lower(), int(x[1])))  # 문제의 조건에 맞게 sort.
+
     arr = []
     for i in answer:
         temp = ""
         for j in i:
             temp += j
         arr.append(temp)
+
     return arr
