bba8f5aeec91a31cbd8a0d2fc77ec251cf9b7167
wooktori <jjun9503@gmail.com>
2026-04-08 20:42:31 +0900

    전력망을 둘로 나누기 수정 및 주석 추가

--- 프로그래머스/javascript/LV2/전력망을 둘로 나누기.js
+++ 프로그래머스/javascript/LV2/전력망을 둘로 나누기.js
@@ -1,36 +1,39 @@
 function solution(n, wires) {
-    let answer = n;
-
-    const graph = Array.from({ length: n + 1 }, () => []);
-    for (const [s, e] of wires) {
-        graph[s].push(e);
-        graph[e].push(s);
+    const graph = Array.from({length:n+1}, ()=>[]);
+    let answer = 1e9
+    
+    for(let i=0;i<wires.length;i++){ // 그래프 생성
+        const [x,y] = wires[i];
+        graph[x].push(y);
+        graph[y].push(x);
     }
-
-    function bfs(start, exclude) {
-        const visited = Array(n + 1).fill(false);
-        const queue = [start];
-        visited[start] = true;
-        visited[exclude] = true; // 제거한 간선의 반대편 노드 차단
-        let count = 1;
-
-        while (queue.length) {
-            const cur = queue.shift();
-            for (const next of graph[cur]) {
-                if (!visited[next]) {
+        
+    function bfs(x,y){ // x-y의 간선을 자르는 것으로 간주
+        const visited = Array(n+1).fill(false);
+        const queue = [1];
+        visited[1] = true;
+        let count = 0;
+        let head = 0;
+        
+        while(queue.length>head){
+            const node = queue[head++];
+            count++;
+            
+            for(const next of graph[node]){
+                if (!visited[next] && !(node === x && next === y) && !(node === y && next === x)) { // 방문하지 않았고, x-y가 아니라면
                     visited[next] = true;
                     queue.push(next);
-                    count++;
                 }
             }
         }
-        return count;
+        return count
     }
-
-    for (const [s, e] of wires) {
-        const count = bfs(s, e);
-        answer = Math.min(answer, Math.abs(n - count * 2));
-    }
-
+        
+    for(let i=0;i<wires.length;i++){
+            const temp = bfs(wires[i][0],wires[i][1]);
+            const value = Math.abs(temp- (n-temp));
+            answer = Math.min(answer, value);
+        }
+        
     return answer;
 }
